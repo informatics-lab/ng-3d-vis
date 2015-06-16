ebbd23d7e6699a48eede6d0a103c980fa85616ed
Rachel Prudden <rachel@Admins-MacBook-Pro-5.local>
2015-06-16 13:51:31 +0100

    Moved shader request logic into a service.

--- components/scene_objects/box/glBox.js
+++ components/scene_objects/box/glBox.js
@@ -3,23 +3,15 @@ angular.module('ngWebglDemo')
     return {
       restrict: 'E',
       require: "^ngWebgl",
+      controller: boxController,
+      controllerAs: 'vm',
       link: function postLink(scope, element, attrs, parentCtrl) {
         function drawBox() {
             var geometry  = new THREE.BoxGeometry( 100, 100, 100 );
 
-            // var materialbackFace = new THREE.ShaderMaterial( {
-            //   vertexShader: document.getElementById( 'vertexShaderBackFace' ).textContent,
-            //   fragmentShader: document.getElementById( 'fragmentShaderBackFace' ).textContent
-            // });
             var materialbackFace = new THREE.ShaderMaterial( {
-              vertexShader: scope.vertexShader,
-              fragmentShader: scope.fragmentShader
-            });
-
-            var lambert = new THREE.MeshLambertMaterial({ 
-              color: 0xff00ff, 
-              shading: THREE.FlatShading, 
-              vertexColors: THREE.VertexColors 
+              vertexShader: scope.vm.vertexShader,
+              fragmentShader: scope.vm.fragmentShader
             });
 
             // Build and add the icosahedron to the scene
@@ -29,49 +21,23 @@ angular.module('ngWebglDemo')
             box.rotation.y = -0.2;
             parentCtrl.addSomething(box);
         }
-
-        function loadShaders() {
-            scope.fragmentLoaded = false;
-            scope.vertexLoaded = false;
-            scope.vertexShader = null;
-            scope.fragmentShader = null;
-            function onVertexLoad() {
-              scope.vertexShader = this.responseText;
-              scope.$broadcast('vertexLoad');
-            }
-            function onFragmentLoad() {
-              scope.fragmentShader = this.responseText;
-              scope.$broadcast('fragmentLoad');
-            }
-
-            var vReq = new XMLHttpRequest();
-            vReq.onload = onVertexLoad;
-            vReq.open("get", 'components/scene_objects/box/box_vertex_shader.c', true);
-            vReq.send();
-
-            var fReq = new XMLHttpRequest();
-            fReq.onload = onFragmentLoad;
-            fReq.open("get", 'components/scene_objects/box/box_fragment_shader.c', true);
-            fReq.send();
-
-            function conditionalBroadcast() {
-              if (scope.vertexLoaded && scope.fragmentLoaded) {
-                scope.$broadcast('shadersLoaded');
-              }
-            }
-            scope.$on('vertexLoad', function(){
-              scope.vertexLoaded = true;
-              conditionalBroadcast();
-            })
-            scope.$on('fragmentLoad', function(){
-              scope.fragmentLoaded = true;
-              conditionalBroadcast();
-            })
-            scope.$on('shadersLoaded', function(){
-              drawBox();
-            })
-        }
-        loadShaders();
+        
+        scope.vm.shaderService.loadShaders();
+        scope.$on('shadersLoaded', function(){
+          scope.vm.setShaders();
+          drawBox();
+        })
       }
     };
   });
+
+function boxController($scope, glBoxShaderRequestService) {
+  var vm = this;
+
+  vm.shaderService = glBoxShaderRequestService;
+
+  vm.setShaders = function() {
+    vm.vertexShader = vm.shaderService.vertexShader;
+    vm.fragmentShader = vm.shaderService.fragmentShader;
+  }
+}
