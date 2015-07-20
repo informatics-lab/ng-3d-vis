8a06047caa1122c86d19c3986d7d1de37945eeb4
Rachel Prudden <rachel@Admins-MacBook-Pro-5.local>
2015-07-20 11:44:18 +0100

    Moved postlink functions out of global scope.

--- components/ngWebgl.js
+++ components/ngWebgl.js
@@ -12,144 +12,140 @@ angular.module('ngWebglDemo')
             controller: webglController,
             controllerAs: 'vm',
             link: {
-                pre: webglPreLink,
-                post: webglPostLink
+                pre: function webglPreLink(scope, element, attrs) {
+                          var scene = new THREE.Scene();
+                          scope.scene = scene;
+                          scope.vm.sceneService.setScene(scene);
+                      },
+                post: function webglPostLink(scope, element, attrs) {
+                          var params = scope.vm.paramService;
+
+                          var contW = scope.width;
+                          var contH = scope.height;
+                          var windowHalfX = contW / 2;
+                          var windowHalfY = contH / 2;
+                          scope.then = Date.now();
+
+
+                          scope.init = function () {
+                              scope.vm.cameraService.setupCamera(
+                                  contW / contH, params.CAMERA_STANDOFF, params.Z_SCALING, scope.datashape
+                              );
+
+                              // Scene
+                              var scene = scope.vm.sceneService.scene;
+                              scope.vm.scene = scene;
+
+                              // Lighting
+
+                              var lightColor = 0xFFFFFF;
+                              var dirLightIntensity = 3;
+                              var dirLight = new THREE.DirectionalLight(lightColor, dirLightIntensity);
+                              dirLight.position.set(0.0, 20.0, 0.0);
+                              scope.vm.dirLight = dirLight;
+                              scope.vm.sceneService.addSomething(dirLight);
+
+                              // Shadow
+                              var canvas = document.createElement('canvas');
+                              canvas.width = 128;
+                              canvas.height = 128;
+
+                              var renderer = new THREE.WebGLRenderer({antialias: true});
+                              renderer.setClearColor("rgb(135, 206, 250)", 1);
+                              renderer.setSize(contW, contH);
+                              scope.vm.renderer = renderer;
+
+                              // element is provided by the angular directive
+                              element[0].appendChild(renderer.domElement);
+
+                              // trackball controls
+                              scope.vm.controls = new THREE.OrbitControls(scope.vm.cameraService.camera)
+                              scope.vm.controls.zoomSpeed *= 1.0;
+                              // scope.vm.controls.damping = 0.5;
+                              //scope.vm.controls.addEventListener( 'change', scope.render );
+
+                              window.addEventListener('resize', scope.onWindowResize, false);
+
+                          };
+
+                          // -----------------------------------
+                          // Event listeners
+                          // -----------------------------------
+                          scope.onWindowResize = function () {
+
+                              scope.resizeCanvas();
+
+                          };
+
+                          // -----------------------------------
+                          // Updates
+                          // -----------------------------------
+                          scope.resizeCanvas = function () {
+
+                              contW = scope.width;
+                              contH = scope.height;
+
+                              windowHalfX = contW / 2;
+                              windowHalfY = contH / 2;
+
+                              if (scope.vm.cameraService.camera) {
+                                  scope.vm.cameraService.camera.aspect = contW / contH;
+                                  scope.vm.cameraService.camera.updateProjectionMatrix();
+                              }
+
+                              if (scope.vm.renderer) {
+                                  scope.vm.renderer.setSize(contW, contH);
+                              }
+
+                          };
+                          // -----------------------------------
+                          // Draw and Animate
+                          // -----------------------------------
+                          scope.animate = function () {
+                              scope.vm.broadcastRender();
+
+                              requestAnimationFrame(scope.animate);
+
+                              var now = Date.now();
+                              var delta = now - scope.then;
+                              if (delta > params.interval) {
+
+                                  // update time stuffs
+                                  scope.then = now - (delta % params.interval);
+
+                                  //update();
+                                  // scope.vm.controls.update();
+                                  scope.render();
+                              }
+
+                          };
+
+                          scope.render = function () {
+
+                              scope.vm.renderer.render(scope.vm.scene, scope.vm.cameraService.camera);
+
+                              //scope.vm.broadcastRender();
+
+                          };
+
+                          // -----------------------------------
+                          // Watches
+                          // -----------------------------------
+                          scope.$watch('width + height', function () {
+
+                              scope.resizeCanvas();
+
+                          });
+
+                          scope.$on('videoLoaded', function () {
+                              // Begin
+                              scope.init();
+                              scope.animate();
+                          })
+                      }
             }
         }
     });
-
-function webglPreLink(scope, element, attrs) {
-    var scene = new THREE.Scene();
-    scope.scene = scene;
-    scope.vm.sceneService.setScene(scene);
-}
-
-function webglPostLink(scope, element, attrs) {
-    var params = scope.vm.paramService;
-
-    var contW = scope.width;
-    var contH = scope.height;
-    var windowHalfX = contW / 2;
-    var windowHalfY = contH / 2;
-    scope.then = Date.now();
-
-
-    scope.init = function () {
-        scope.vm.cameraService.setupCamera(
-            contW / contH, params.CAMERA_STANDOFF, params.Z_SCALING, scope.datashape
-        );
-
-        // Scene
-        var scene = scope.vm.sceneService.scene;
-        scope.vm.scene = scene;
-
-        // Lighting
-
-        var lightColor = 0xFFFFFF;
-        var dirLightIntensity = 3;
-        var dirLight = new THREE.DirectionalLight(lightColor, dirLightIntensity);
-        dirLight.position.set(0.0, 20.0, 0.0);
-        scope.vm.dirLight = dirLight;
-        scope.vm.sceneService.addSomething(dirLight);
-
-        // Shadow
-        var canvas = document.createElement('canvas');
-        canvas.width = 128;
-        canvas.height = 128;
-
-        var renderer = new THREE.WebGLRenderer({antialias: true});
-        renderer.setClearColor("rgb(135, 206, 250)", 1);
-        renderer.setSize(contW, contH);
-        scope.vm.renderer = renderer;
-
-        // element is provided by the angular directive
-        element[0].appendChild(renderer.domElement);
-
-        // trackball controls
-        scope.vm.controls = new THREE.OrbitControls(scope.vm.cameraService.camera)
-        scope.vm.controls.zoomSpeed *= 1.0;
-        // scope.vm.controls.damping = 0.5;
-        //scope.vm.controls.addEventListener( 'change', scope.render );
-
-        window.addEventListener('resize', scope.onWindowResize, false);
-
-    };
-
-    // -----------------------------------
-    // Event listeners
-    // -----------------------------------
-    scope.onWindowResize = function () {
-
-        scope.resizeCanvas();
-
-    };
-
-    // -----------------------------------
-    // Updates
-    // -----------------------------------
-    scope.resizeCanvas = function () {
-
-        contW = scope.width;
-        contH = scope.height;
-
-        windowHalfX = contW / 2;
-        windowHalfY = contH / 2;
-
-        if (scope.vm.cameraService.camera) {
-            scope.vm.cameraService.camera.aspect = contW / contH;
-            scope.vm.cameraService.camera.updateProjectionMatrix();
-        }
-
-        if (scope.vm.renderer) {
-            scope.vm.renderer.setSize(contW, contH);
-        }
-
-    };
-    // -----------------------------------
-    // Draw and Animate
-    // -----------------------------------
-    scope.animate = function () {
-        scope.vm.broadcastRender();
-
-        requestAnimationFrame(scope.animate);
-
-        var now = Date.now();
-        var delta = now - scope.then;
-        if (delta > params.interval) {
-
-            // update time stuffs
-            scope.then = now - (delta % params.interval);
-
-            //update();
-            // scope.vm.controls.update();
-            scope.render();
-        }
-
-    };
-
-    scope.render = function () {
-
-        scope.vm.renderer.render(scope.vm.scene, scope.vm.cameraService.camera);
-
-        //scope.vm.broadcastRender();
-
-    };
-
-    // -----------------------------------
-    // Watches
-    // -----------------------------------
-    scope.$watch('width + height', function () {
-
-        scope.resizeCanvas();
-
-    });
-
-    scope.$on('videoLoaded', function () {
-        // Begin
-        scope.init();
-        scope.animate();
-    })
-};
 
 function webglController($scope, $rootScope, glSceneService, glSkyboxParameterService, glVideoDataModelService, glCameraModelService) {
     var vm = this;
--- components/scene_objects/skybox/glSkybox.js
+++ components/scene_objects/skybox/glSkybox.js
@@ -5,164 +5,162 @@ angular.module('ngWebglDemo')
       require: "^ngWebgl",
       controller: skyboxController,
       controllerAs: 'vm',
-      link: postLink,
+      link: function postLink(scope, element, attrs, parentCtrl) {
+
+                function setDataTexType(mipMapTex){
+                  if (mipMapTex){
+                          scope.vm.dataTexture.generateMipmaps = true;
+                          scope.vm.dataTexture.magFilter = THREE.LinearFilter;
+                          scope.vm.dataTexture.minFilter = THREE.LinearMipMapLinearFilter;
+                  }else{
+                          scope.vm.dataTexture.generateMipmaps = false;
+                          scope.vm.dataTexture.magFilter = THREE.NearestFilter;
+                          scope.vm.dataTexture.minFilter = THREE.NearestFilter;
+                  };
+                  scope.vm.dataTexture.needsUpdate = true;
+                }
+
+                function drawBox() {
+                    var params = scope.vm.paramService;
+                    var dims = scope.dims;
+                    var videoImage = scope.vm.videoImage;
+                    var shaders = scope.vm.shaders;
+                    var dirLight = parentCtrl.dirLight;
+
+                    var boxDims = new THREE.Vector3(dims.datashape.x ,
+                                                  dims.datashape.z*params.Z_SCALING,
+                                                  dims.datashape.y);
+                    var boxGeometry = new THREE.BoxGeometry(boxDims.x, boxDims.y, boxDims.z); // the block to render inside
+                    boxGeometry.doubleSided = true;
+
+                    var dataTexture = new THREE.Texture( videoImage );
+                    scope.vm.dataTexture = dataTexture;
+
+                    setDataTexType(params.mipMapTex); // set mip mapping on or off
+
+                    /*************** add aquarium outline **/
+                    var boxOutlineMesh = new THREE.Mesh( boxGeometry );
+                    var boxOutLine = new THREE.BoxHelper( boxOutlineMesh );
+                    boxOutLine.material.color.set( "#000033" );
+                    parentCtrl.addSomething( boxOutLine );
+
+                    /*** first pass ***/
+                    var materialbackFace = new THREE.ShaderMaterial( {
+                        vertexShader: shaders.vertex_shader_back_face,
+                        fragmentShader: shaders.fragment_shader_back_face,
+                        side: THREE.BackSide,
+                        uniforms: {dimensions: {type: "v3", value: boxDims}}
+                    });
+
+                    var meshBackFace = new THREE.Mesh( boxGeometry, materialbackFace );
+                    
+                    var sceneBackFace = new THREE.Scene();
+                    sceneBackFace.add( meshBackFace );
+                    scope.vm.sceneBackFace = sceneBackFace;
+                    
+                    // get the "colour" coords we just made, as a texture
+                    var backFaceTexture = new THREE.WebGLRenderTarget(  window.innerWidth/params.downScaling,
+                                                                     window.innerHeight/params.downScaling,
+                                                             { minFilter: THREE.NearestFilter,
+                                                               magFilter: THREE.NearestFilter,
+                                                               format: THREE.RGBFormat,
+                                                               type: THREE.FloatType } );
+                    scope.vm.backFaceTexture = backFaceTexture;
+                    backFaceTexture.wrapS = backFaceTexture.wrapT = THREE.ClampToEdgeWrapping;    
+                    
+                    /*** second pass ***/
+                    var uniforms = { backFaceTexture: { type: "t", value: backFaceTexture },
+                                         dataTexture: { type: "t", value: scope.vm.dataTexture },
+                                         lightPosition: { type: "v3", value: dirLight.position},
+                                         lightColor: { type: "v3", value: {x: dirLight.color.r, y:dirLight.color.g, z:dirLight.color.b}},
+                                         lightIntensity: {type: "1f", value: dirLight.intensity},
+                                         steps : {type: "1f" , value: params.nSteps}, // so we know how long to make in incriment 
+                                         shadeSteps : {type: "1f" , value: params.shadeSteps},
+                                         alphaCorrection : {type: "1f" , value: params.alphaCorrection },
+                                         ambience : {type: "1f", value: params.ambience},
+                                         dataShape: {type: "v3", value: dims.datashape},
+                                         texShape: {type: "v2", value: dims.textureshape},
+                                         dimensions: {type: "v3", value: boxDims}
+                                     };
+
+                    var materialRayMarch = new THREE.ShaderMaterial( {
+                        vertexShader: shaders.vertex_shader_ray_march,
+                        fragmentShader: shaders.fragment_shader_ray_march,
+                        side: THREE.FrontSide,
+                        uniforms: uniforms
+                    });
+                    // materialRayMarch.transparent = true;
+                    
+                    var sceneRayMarch = new THREE.Scene();
+                    var meshRayMarch = new THREE.Mesh( boxGeometry, materialRayMarch );
+                    sceneRayMarch.add( meshRayMarch );
+                    scope.vm.sceneRayMarch = sceneRayMarch;
+
+                    var rayMarchTexture = new THREE.WebGLRenderTarget(  window.innerWidth/params.downScaling,
+                                                                     window.innerHeight/params.downScaling,
+                                                             { minFilter: THREE.LinearFilter,
+                                                               magFilter: THREE.LinearFilter,
+                                                               format: THREE.RGBAFormat,
+                                                               type: THREE.FloatType } );
+                    scope.vm.rayMarchTexture = rayMarchTexture;
+                    rayMarchTexture.wrapS = rayMarchTexture.wrapT = THREE.ClampToEdgeWrapping;
+
+                    /*************** Resample ray marching ****/
+                    var uniforms2 = {rayMarchTexture: {type: "t", value: rayMarchTexture}};
+                    materialResampledRayMarch = new THREE.ShaderMaterial( {
+                        vertexShader: shaders.vertex_shader_resample,
+                        fragmentShader: shaders.fragment_shader_resample,
+                        side: THREE.FrontSide,
+                        uniforms: uniforms2
+                    });
+                    materialResampledRayMarch.transparent = true;
+                    var meshResampledRayMarch = new THREE.Mesh( boxGeometry, materialResampledRayMarch );
+
+                    parentCtrl.addSomething(meshResampledRayMarch);
+
+                    
+                    scope.vm.ready = true;
+                }
+
+                function conditionalBroadcast() {
+                  if (scope.vm.gotShaders && scope.vm.gotVideo) {
+                    scope.$broadcast('skyboxReady');
+                    scope.vm.gotShaders = false;
+                    scope.vm.gotVideo = false;
+                  }
+                }
+                
+                scope.vm.fetchShaders();
+                scope.$on('shadersLoaded', function() {
+                  scope.vm.setShaders();
+                  scope.vm.gotShaders = true;
+                  conditionalBroadcast();
+                })
+                scope.$on('videoLoaded', function() {
+                  scope.vm.setVideoImage();
+                  scope.vm.gotVideo = true;
+                  conditionalBroadcast();
+                })
+                scope.$on('skyboxReady', function() {
+                  drawBox();
+                })
+                scope.$on('render', function() {
+                  if (scope.vm.ready) {
+                    //scope.vm.dataTexture.needsUpdate = true;
+                    parentCtrl.renderer.render( scope.vm.sceneBackFace, parentCtrl.cameraService.camera, scope.vm.backFaceTexture, true);
+                    parentCtrl.renderer.render( scope.vm.sceneRayMarch, parentCtrl.cameraService.camera, scope.vm.rayMarchTexture, true );
+                  }
+                })
+                scope.$on('videoUpdate', function() {
+                  scope.vm.dataTexture.needsUpdate = true;
+                })
+
+              },
       scope: {
         'dims': '='
       }
     };
   });
-
-function postLink(scope, element, attrs, parentCtrl) {
-
-  function setDataTexType(mipMapTex){
-    if (mipMapTex){
-            scope.vm.dataTexture.generateMipmaps = true;
-            scope.vm.dataTexture.magFilter = THREE.LinearFilter;
-            scope.vm.dataTexture.minFilter = THREE.LinearMipMapLinearFilter;
-    }else{
-            scope.vm.dataTexture.generateMipmaps = false;
-            scope.vm.dataTexture.magFilter = THREE.NearestFilter;
-            scope.vm.dataTexture.minFilter = THREE.NearestFilter;
-    };
-    scope.vm.dataTexture.needsUpdate = true;
-  }
-
-  function drawBox() {
-      var params = scope.vm.paramService;
-      var dims = scope.dims;
-      var videoImage = scope.vm.videoImage;
-      var shaders = scope.vm.shaders;
-      var dirLight = parentCtrl.dirLight;
-
-      var boxDims = new THREE.Vector3(dims.datashape.x ,
-                                    dims.datashape.z*params.Z_SCALING,
-                                    dims.datashape.y);
-      var boxGeometry = new THREE.BoxGeometry(boxDims.x, boxDims.y, boxDims.z); // the block to render inside
-      boxGeometry.doubleSided = true;
-
-      var dataTexture = new THREE.Texture( videoImage );
-      scope.vm.dataTexture = dataTexture;
-
-      setDataTexType(params.mipMapTex); // set mip mapping on or off
-
-      /*************** add aquarium outline **/
-      var boxOutlineMesh = new THREE.Mesh( boxGeometry );
-      var boxOutLine = new THREE.BoxHelper( boxOutlineMesh );
-      boxOutLine.material.color.set( "#000033" );
-      parentCtrl.addSomething( boxOutLine );
-
-      /*** first pass ***/
-      var materialbackFace = new THREE.ShaderMaterial( {
-          vertexShader: shaders.vertex_shader_back_face,
-          fragmentShader: shaders.fragment_shader_back_face,
-          side: THREE.BackSide,
-          uniforms: {dimensions: {type: "v3", value: boxDims}}
-      });
-
-      var meshBackFace = new THREE.Mesh( boxGeometry, materialbackFace );
-      
-      var sceneBackFace = new THREE.Scene();
-      sceneBackFace.add( meshBackFace );
-      scope.vm.sceneBackFace = sceneBackFace;
-      
-      // get the "colour" coords we just made, as a texture
-      var backFaceTexture = new THREE.WebGLRenderTarget(  window.innerWidth/params.downScaling,
-                                                       window.innerHeight/params.downScaling,
-                                               { minFilter: THREE.NearestFilter,
-                                                 magFilter: THREE.NearestFilter,
-                                                 format: THREE.RGBFormat,
-                                                 type: THREE.FloatType } );
-      scope.vm.backFaceTexture = backFaceTexture;
-      backFaceTexture.wrapS = backFaceTexture.wrapT = THREE.ClampToEdgeWrapping;    
-      
-      /*** second pass ***/
-      var uniforms = { backFaceTexture: { type: "t", value: backFaceTexture },
-                           dataTexture: { type: "t", value: scope.vm.dataTexture },
-                           lightPosition: { type: "v3", value: dirLight.position},
-                           lightColor: { type: "v3", value: {x: dirLight.color.r, y:dirLight.color.g, z:dirLight.color.b}},
-                           lightIntensity: {type: "1f", value: dirLight.intensity},
-                           steps : {type: "1f" , value: params.nSteps}, // so we know how long to make in incriment 
-                           shadeSteps : {type: "1f" , value: params.shadeSteps},
-                           alphaCorrection : {type: "1f" , value: params.alphaCorrection },
-                           ambience : {type: "1f", value: params.ambience},
-                           dataShape: {type: "v3", value: dims.datashape},
-                           texShape: {type: "v2", value: dims.textureshape},
-                           dimensions: {type: "v3", value: boxDims}
-                       };
-
-      var materialRayMarch = new THREE.ShaderMaterial( {
-          vertexShader: shaders.vertex_shader_ray_march,
-          fragmentShader: shaders.fragment_shader_ray_march,
-          side: THREE.FrontSide,
-          uniforms: uniforms
-      });
-      // materialRayMarch.transparent = true;
-      
-      var sceneRayMarch = new THREE.Scene();
-      var meshRayMarch = new THREE.Mesh( boxGeometry, materialRayMarch );
-      sceneRayMarch.add( meshRayMarch );
-      scope.vm.sceneRayMarch = sceneRayMarch;
-
-      var rayMarchTexture = new THREE.WebGLRenderTarget(  window.innerWidth/params.downScaling,
-                                                       window.innerHeight/params.downScaling,
-                                               { minFilter: THREE.LinearFilter,
-                                                 magFilter: THREE.LinearFilter,
-                                                 format: THREE.RGBAFormat,
-                                                 type: THREE.FloatType } );
-      scope.vm.rayMarchTexture = rayMarchTexture;
-      rayMarchTexture.wrapS = rayMarchTexture.wrapT = THREE.ClampToEdgeWrapping;
-
-      /*************** Resample ray marching ****/
-      var uniforms2 = {rayMarchTexture: {type: "t", value: rayMarchTexture}};
-      materialResampledRayMarch = new THREE.ShaderMaterial( {
-          vertexShader: shaders.vertex_shader_resample,
-          fragmentShader: shaders.fragment_shader_resample,
-          side: THREE.FrontSide,
-          uniforms: uniforms2
-      });
-      materialResampledRayMarch.transparent = true;
-      var meshResampledRayMarch = new THREE.Mesh( boxGeometry, materialResampledRayMarch );
-
-      parentCtrl.addSomething(meshResampledRayMarch);
-
-      
-      scope.vm.ready = true;
-  }
-
-  function conditionalBroadcast() {
-    if (scope.vm.gotShaders && scope.vm.gotVideo) {
-      scope.$broadcast('skyboxReady');
-      scope.vm.gotShaders = false;
-      scope.vm.gotVideo = false;
-    }
-  }
-  
-  scope.vm.fetchShaders();
-  scope.$on('shadersLoaded', function() {
-    scope.vm.setShaders();
-    scope.vm.gotShaders = true;
-    conditionalBroadcast();
-  })
-  scope.$on('videoLoaded', function() {
-    scope.vm.setVideoImage();
-    scope.vm.gotVideo = true;
-    conditionalBroadcast();
-  })
-  scope.$on('skyboxReady', function() {
-    drawBox();
-  })
-  scope.$on('render', function() {
-    if (scope.vm.ready) {
-      //scope.vm.dataTexture.needsUpdate = true;
-      parentCtrl.renderer.render( scope.vm.sceneBackFace, parentCtrl.cameraService.camera, scope.vm.backFaceTexture, true);
-      parentCtrl.renderer.render( scope.vm.sceneRayMarch, parentCtrl.cameraService.camera, scope.vm.rayMarchTexture, true );
-    }
-  })
-  scope.$on('videoUpdate', function() {
-    scope.vm.dataTexture.needsUpdate = true;
-  })
-
-}
 
 function skyboxController($scope, glShaderRequestService, glVideoDataModelService, glSkyboxParameterService) {
   var vm = this;
