36d162f6f3240c8e98570ba02d908a892d3b865f
Thomas Powell <t.powell.meto@gmail.com>
2015-07-22 17:18:18 +0100

    syncing issue

--- components/controls/glMobileSocketService.js
+++ components/controls/glMobileSocketService.js
@@ -7,33 +7,35 @@ angular.module('mobileApp')
         vm.connectedToRoom = false;
         vm.connectionStatus = "disconnected";
 
-        vm.socket = io.connect("http://sync.3dvis.informaticslab.co.uk/");
-
-
         vm.roomId = prompt("Enter sync ID");
         if(vm.roomId) {
+
             vm.socket.emit('subscribe', vm.roomId);
             vm.connectionStatus = "waiting";
+            vm.socket = io.connect("http://sync.3dvis.informaticslab.co.uk/");
+
+            vm.socket.on('subscription', function (data) {
+                vm.connectedToRoom = true;
+                vm.roomId = data.roomId;
+                console.log('roomId set to : ' + vm.roomId);
+
+                //TODO swap alerts out for modals
+                if (data.participants == 1) {
+                    alert("open main app in a desktop first");
+                } else if (data.participants > 1) {
+                    vm.connectionStatus = "connected";
+                }
+            });
+
+            vm.send = function(message) {
+                vm.socket.emit('', {
+                    room : roomId,
+                    message : message
+                });
+            };
+
         }
 
-        vm.socket.on('subscription', function (data) {
-            vm.connectedToRoom = true;
-            vm.roomId = data.roomId;
-            console.log('roomId set to : ' + vm.roomId);
 
-            //TODO swap alerts out for modals
-            if (data.participants == 1) {
-                alert("open main app in a desktop first");
-            } else if (data.participants > 1) {
-                vm.connectionStatus = "connected";
-            }
-        });
-
-        vm.send = function(message) {
-            vm.socket.emit('', {
-                room : roomId,
-                message : message
-            });
-        };
 
     });
