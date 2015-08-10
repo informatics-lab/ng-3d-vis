2430e225cf7f571e9091ad3a78bd806d2c10eff9
Rachel Prudden <rachel@Admins-MacBook-Pro-5.local>
2015-08-10 16:30:25 +0100

    Improvements to reverse location search

--- components/location_search/glReverseLocationSearch.js
+++ components/location_search/glReverseLocationSearch.js
@@ -20,24 +20,33 @@ function reverseLocSearchPostLink(scope, element, attrs) {
     scope.vm.place_search = new google.maps.places.PlacesService(document.createElement('div'));
 }
 
+function stringify(latlon) {
+    return latlon.lat.toFixed(1) + ', ' + latlon.lon.toFixed(1);
+}
+
 function reverseLocSearchController($scope, $interval, glCoordService, glCameraService) {
     var vm = this;
 
     var checkPosition = function() {
         if(vm.position && glCameraService.camera.position.equals(vm.position)) {
-            //console.log(vm.position);
             var loc = vm.coordService.lookupLatLon(vm.position);
             console.log(loc);
             vm.latlon = loc;
             var request = {
                 location: new google.maps.LatLng(loc.lat, loc.lon),
-                radius: '200'//,
-                //types: ['(cities)']
+                radius: '100'
               };
             vm.place_search.nearbySearch(request, callback);
             function callback(results, status){
-                console.log(results);
-                vm.loc_input.value = results[0].name;
+                var filterfn = function(el){return el.types.indexOf("political") > -1};
+                var political_results = results.filter(filterfn);
+                console.log(political_results);
+                try {
+                    vm.loc_input.value = political_results[0].name;
+                }
+                catch (err) {
+                    vm.loc_input.value = stringify(vm.latlon);
+                }
             }
         }
         vm.position = new THREE.Vector3(glCameraService.camera.position.x,glCameraService.camera.position.y,glCameraService.camera.position.z);
