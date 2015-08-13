546926b0a56b526bc01dfa5b57133d33e9dcb849
Rachel Prudden <rachel@Admins-MacBook-Pro-5.local>
2015-08-13 12:04:51 +0100

    Added crucial file and made event fire from the right place.

--- components/location_search/glReverseLocationSearch.js
+++ components/location_search/glReverseLocationSearch.js
@@ -26,12 +26,17 @@ function stringify(latlon) {
 
 function reverseLocSearchController($scope, $rootScope, $interval, glCoordService, glCameraService) {
     var vm = this;
+    vm.prev = {lat:0,lon:0};
 
     var checkPosition = function() {
         if(vm.position && glCameraService.camera.position.equals(vm.position)) {
             var loc = vm.coordService.lookupLatLon(vm.position);
             console.log(loc);
             vm.latlon = loc;
+            if ((vm.latlon.lat !== vm.prev.lat) || (vm.latlon.lon !== vm.prev.lon)){
+                $rootScope.$broadcast('position_change', {position:vm.latlon});
+            }
+            vm.prev = vm.latlon;
             var request = {
                 location: new google.maps.LatLng(loc.lat, loc.lon),
                 radius: '100'
@@ -42,14 +47,10 @@ function reverseLocSearchController($scope, $rootScope, $interval, glCoordServic
                 var political_results = results.filter(filterfn);
                 try {
                     vm.loc_input.value = political_results[0].name;
-                    if ((vm.latlon.lat !== vm.prev.lat) || (vm.latlon.lon !== vm.prev.lon)){
-                        $rootScope.$broadcast('position_change', {position:vm.latlon});
-                    }
                 }
                 catch (err) {
                     vm.loc_input.value = stringify(vm.latlon);
                 }
-                vm.prev = vm.latlon;
             }
         }
         vm.position = new THREE.Vector3(glCameraService.camera.position.x,glCameraService.camera.position.y,glCameraService.camera.position.z);
